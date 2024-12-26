15dc591f75469edbd0acb369803de9bfba0c0eb1
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 <piritaminkkinen@gmail.com>
2024-12-26 02:46:38 +0100

    Initial Commit

---
+++ Tests/PPM data compressionTests/PPM_data_compressionTests.swift
@@ -0,0 +1,6 @@
+import Testing
+@testable import PPM_data_compression
+
+@Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
