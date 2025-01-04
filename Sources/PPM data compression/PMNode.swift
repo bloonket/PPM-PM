758c463ce4cc13a563563a5658574601a3aed2ef
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 <piritaminkkinen@gmail.com>
2025-01-04 04:42:34 +0100

    implemented modifier manipluation in the Node

--- Sources/PPM data compression/PMNode.swift
+++ Sources/PPM data compression/PMNode.swift
@@ -15,7 +15,7 @@ class PMNode {
 
     var children: [PMNode?]
     var frequencies: [Int]
-    var modifiers: [Int]
+    var modifiers: [Float] //TODO: consider different float values
     var isConsidered: [Bool] // Tracks if a symbol is "likely"
 
     init(alphabetSize: Int?) {
@@ -23,7 +23,7 @@ class PMNode {
             // Pre-allocate arrays for known alphabet size
             self.children = Array<PMNode?>(repeating: nil, count: alphabetSize)
             self.frequencies = Array<Int>(repeating: 0, count: alphabetSize)
-            self.modifiers = Array<Int>(repeating: 0, count: alphabetSize)
+            self.modifiers = Array<Float>(repeating: 1, count: alphabetSize)
             self.isConsidered = Array<Bool>(repeating: true, count: alphabetSize)
         } else {
             // Use empty arrays for unknown alphabet size
@@ -47,13 +47,35 @@ class PMNode {
     
     
     func updateFrequency(index: Int) {
-        // Update frequency
-        frequencies[index] += 1 //TODO: change the default value to more calculated one
-        
-        // Update topFrequency and bottomFrequency
-        if frequencies[index] > topFrequency {
+        let increase = Int(1 * modifiers[index])  //TODO: change the default value to more calculated one
+        if increase + frequencies[index] > topFrequency {
+            frequencies[index] = topFrequency + 1
             topFrequency = frequencies[index]
         }
+        else {
+            frequencies[index] += increase
+        }
+        
+        if !isConsidered[index] {
+            if frequencies[index] > bottomFrequency{
+                //after being promoted to one of the considered ones, remove modifier
+                isConsidered[index] = true
+                modifiers[index] = 1
+                adjustThreshold()
+            }
+            else{
+                //TODO: add modifier increase here
+            }
+            //increase modifier slowly, since it being bigger than 1 already causes the increase to frequencies to be exponential
+            //but tbh modifier should be increased more the higher it is, so if the difference is very big (like in big data) the newly frequent letter can be bounced to top as fast as needed
+            //i guess i could just double the modifier every time :D
+        }
+        //frecuency gets updated by 1 * modifier, the modifier is 1 first and will gratually rise to idk to what, it needs to be something meanigfull since indexes are ints at least for now, i need to consider tho having them as floats and if that even is possible :)
+        // when modifier doesnt get used it decause a bit, i guess it would need to decay more and more to 1, tbh i need to consider if the decay is not taking too much to do, since it would require me to iterate trough 1 array everytime i do adjustment, i think its not worth it, i think its ok if the boosting effect stays in the memory, since it will eventually get reset and the the "error" will be corrected when the number falls from the considered list if it is not useful
+        //if the frecuency causes the node to go from being not considered to considered, the modifier would need to go to 1 instatntly
+        //the modifier only increases if the charachter is not considered
+        
+        //TODO: check if this is needed
         if isConsidered[index], frequencies[index] < bottomFrequency {
             bottomFrequency = frequencies[index]
         }
